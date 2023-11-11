f54e046e87699e41e773176c937b445e35b1607a
asilbek <asilbekmirolimov@gmail.com>
2023-11-11 17:24:36 +0500

    initial commit

--- posts/views.py
+++ posts/views.py
@@ -13,6 +13,25 @@ def posts(request):
     return render(request, template_name='index.html', context=context)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'post_detail.html', {'post': post})
